a6dd7c01edbd9383c456279a2f72dbe16a99dadf
SermilionRestless <scat95@bk.ru>
2017-12-24 00:06:16 -0800

    Добавили ссылку на кнопку и добавили listener для кнопки чтобы показывать тост с тем что было введено в поле email

--- app/src/main/java/net/oneread/redditbros/LoginActivity.java
+++ app/src/main/java/net/oneread/redditbros/LoginActivity.java
@@ -2,6 +2,10 @@ package net.oneread.redditbros;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final EditText emailView = findViewById(R.id.email);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, emailView.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
